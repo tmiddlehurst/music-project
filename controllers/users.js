87b4bb5f9b0c4c0fdc35b99d109fea07ebbb00af
Tom Middlehurst <tom.middlehurst@hotmail.co.uk>
2017-02-17 11:15:40 +0000

    Written skeleton for creating users. I need to populate user and users files

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+var User = require ('../models/user.js')
+
+function showUser(req, res) {
+	Band.findById(req.params.id , function(err, user) {
+		if(!user) return res.status(404).send("Not found");
+		if(err) return res.status(500).send(err);
+		res.render("users/show" , {
+			title: "User",
+			user: user
+		});
+	}); 
+}
+									// TESTED
+
+
+function newUser(req, res) {
+	var newUser = {
+		username: "",
+		email: "",
+		profileImage: ""
+	};
+	res.render("users/new" , {
+		title: "New User",
+		user: newUser
+	}); 
+}
+
+function createUser(req, res) {
+	console.log(req.body);
+	Band.create(req.body, function(err, band) {
+		if(err) return res.status(500).send(err);
+		res.redirect("/");
+	});
+}
